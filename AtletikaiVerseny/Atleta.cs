7e564338f2ee7352b4fac8dc7e1d4aeb4ad40da6
leveses <boda.zsombor10@gmail.com>
2020-10-13 08:47:38 +0200

    Beolvasás

---
+++ AtletikaiVerseny/Atleta.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtletikaiVerseny
+{
+    class Atleta
+    {
+        public string Rajtszam { get; private set; }
+        public string VezNev { get; private set; }
+        public string KerNev { get; private set; }
+        public string Egyesulet { get; private set; }
+        public int Ugras { get; private set; }
+
+        public Atleta(string n)
+        {
+            string[] sor = n.Split(';');
+            sor[0] = Rajtszam;
+            sor[1] = VezNev;
+            sor[2] = KerNev;
+            sor[3] = Egyesulet;
+            
+        }
+    }
+}
--- AtletikaiVerseny/Program.cs
+++ AtletikaiVerseny/Program.cs
@@ -3,13 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Diagnostics.CodeAnalysis;
 
 namespace AtletikaiVerseny
 {
     class Program
     {
+        static List<Atleta> lista = new List<Atleta>();
+        static List<string> nevek = new List<string>();
+        static Dictionary<string, int> konyvtar = new Dictionary<string, int>();
         static void Main(string[] args)
         {
+            Beolvas();
+            Console.WriteLine("1. feladat: adatok beolvasása");
+            Console.WriteLine();
+            Feladat02();
+            Console.WriteLine();
+            Feladat03();
+            Console.WriteLine();
+            Feladat04();
+            Console.WriteLine();
+            Feladat05();
+            Console.WriteLine();
+            Feladat06();
+
+            
+            Console.ReadKey();
         }
+
+        
+
+        private static void Beolvas()
+        {
+            StreamReader file = new StreamReader("tavol.csv");
+            while (!file.EndOfStream)
+            {
+                lista.Add(new Atleta(file.ReadLine()));
+            }
+            file.Close();
+        }
+        
     }
 }
